f8fdb03e45b1b4d4142719393a3e19b5e2c14ebe
com <mihailo.radovic@gamecredits.com>
2018-03-19 14:55:57 +0100

    change external api strucutre

---
+++ src/main/java/com/example/demo/external_api/wheather_api/domain/Wheather.java
@@ -0,0 +1,4 @@
+package com.example.demo.external_api.wheather_api.domain;
+
+public class Wheather {
+}
---
+++ src/main/java/com/example/demo/external_api/wheather_api/services/WheatherApiServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.demo.external_api.wheather_api.services;
+
+public class WheatherApiServiceImpl implements WheatherApiService {
+}
